b072836bf6ddf46e15616c8a8735b3df7459ea40
Maksym Kolodiy <mak.kolodiy@gmail.com>
2022-04-02 18:20:57 +0200

    chore: Removed tsdx and switched to vite fully

---
+++ packages/modeler/vite.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: 'src/index.ts',
+      name: 'MyLib',
+      formats: ['es'],
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['jointjs'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          jointjs: 'joint',
+        },
+      },
+    },
+  },
+});
